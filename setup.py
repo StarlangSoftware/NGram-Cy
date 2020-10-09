8ee8a8bbac90682380da1b973df34150c2673ddd
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2020-10-09 22:09:14 +0300

    Added LaplaceSmoothing.pxd.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     ext_modules=cythonize(["NGram/*.pyx"],
                           compiler_directives={'language_level': "3"}),
     name='NlpToolkit-NGram-Cy',
-    version='1.0.1',
+    version='1.0.2',
     packages=['NGram'],
     package_data={'NGram': ['*.pxd', '*.pyx', '*.c']},
     url='https://github.com/olcaytaner/NGram-Cy',

e683e9b7fbce420d665a7df46a1bbe2b7bd11709
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-30 21:31:48 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 from Cython.Build import cythonize
 
 setup(
     ext_modules=cythonize(["NGram/*.pyx"],
                           compiler_directives={'language_level': "3"}),
     name='NlpToolkit-NGram-Cy',
-    version='1.0.6',
+    version='1.0.7',
     packages=['NGram'],
     package_data={'NGram': ['*.pxd', '*.pyx', '*.c', '*.py']},
     url='https://github.com/StarlangSoftware/NGram-Cy',
@@ -13,5 +17,7 @@ setup(
     author='olcaytaner',
     author_email='olcay.yildiz@ozyegin.edu.tr',
     description='NGram library',
-    install_requires=['NlpToolkit-DataStructure-Cy', 'NlpToolkit-Sampling-Cy']
+    install_requires=['NlpToolkit-DataStructure-Cy', 'NlpToolkit-Sampling-Cy'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
